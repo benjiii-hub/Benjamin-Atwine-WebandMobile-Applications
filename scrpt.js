6c630ffd7d4b9d9a70cd02d6a04072d7bb0f1eb6
benjiiii <kline9341@gmail.com>
2025-10-08 08:39:53 +0300

    Add files via upload

---
+++ scrpt.js
@@ -0,0 +1,26 @@
+// VARIABLES
+
+// let age = 14;
+// age = 16;
+// const my_name = "Og";
+// my_name = "Ok";
+// var school = "UCU";
+
+let course_work_mark = 25;
+let ExamMarks = 50;
+// const myName = "Okg"
+
+let finalMark = course_work_mark + ExamMarks;
+
+console.log("The Final Score is " + finalMark);
+console.log("When Divided it equates to " + (course_work_mark / ExamMarks))
+
+console.log("When Multiplied it results in " + (course_work_mark * ExamMarks))
+
+
+const surname = "Okg";
+const othername = "Mike";
+
+const full_name = othername + surname;
+console.log("His N ame is " + full_name);
+console.log("Welcome Mr. " + othername + surname);
