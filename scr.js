7825085a89ca4e431bc8025768af94bee9c3c071
benjiiii <kline9341@gmail.com>
2025-10-12 18:29:31 +0300

    Add files via upload

---
+++ scr.js
@@ -0,0 +1,6 @@
+const surname = "Atwine";
+const othername = "Benjamin";
+
+const full_name = othername + surname;
+console.log("This Portfolio belongs to " + full_name);
+console.log("Welcome Mr. " + othername + surname);
